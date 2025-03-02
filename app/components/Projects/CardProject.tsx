36c5b8929ed5cdf0ca4d7089b1d19452cd3bd880
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-02 17:48:39 -0300

    styles(Projects): Add styles to Projects component

--- app/components/Projects/CardProject.tsx
+++ app/components/Projects/CardProject.tsx
@@ -4,11 +4,12 @@ import { RiNextjsLine as NextIcon } from "react-icons/ri";
 import { FaReact as ReactIcon } from "react-icons/fa";
 import { FaCss3 as CssIcon } from "react-icons/fa";
 import { SiMongodb as MongoIcon} from "react-icons/si";
+import styles from '../../styles/Projects.module.css'
 export default function CardProject(){
     return(
-        <div className="card">
-            <div className="image"></div>
-            <ul className="tags">
+        <div className={styles.card}>
+            <div className={styles.image}></div>
+            <ul className={styles.tags}>
                 <li>
                     <NextIcon/>
                     <p>Next</p>
@@ -28,13 +29,13 @@ export default function CardProject(){
             </ul>
             <h4>Weather App</h4>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur aspernatur atque quo quia molestias officiis quae? Minima perspiciatis libero laboriosam ullam, repellat molestias magni culpa at facilis voluptate hic illo.</p>
-            <div className="button">
-                <button className="button__demo">
-                    <WebIcon/>
+            <div className={styles.button}>
+                <button className={styles.button__demo}>
+                    <WebIcon className={styles.button_icon}/>
                     <p>Demo</p>
                 </button>
-                <button className="button__github">
-                    <GithubIcon/>
+                <button className={styles.button__github}>
+                    <GithubIcon className={styles.button_icon}/>
                     <p>Github</p>
                 </button>
             </div>
--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,9 +1,13 @@
 import CardProject from './CardProject'
+import styles from '../../styles/Projects.module.css'
 export default function Projects(){
     return(
-        <section className="containerProjects">
+        <section className={styles.containerProjects}>
             <h2>Projects</h2>
-            <div className="projects">
+            <div className={styles.projects}>
+                <CardProject/>
+                <CardProject/>
+                <CardProject/>
                 <CardProject/>
             </div>
         </section>
