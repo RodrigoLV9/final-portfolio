a7809107e63215edb95a1d62b0251510fbb22ee8
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-04 00:20:54 -0300

    feat(logo): Add logo for tab in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children}:{children:React.ReactNode}){
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <link rel="shortcut icon" href="/logo-portfolio-mini.png" type="image/x-icon"/>
+      </head>
       <body>
         <Header/>
         {children}
