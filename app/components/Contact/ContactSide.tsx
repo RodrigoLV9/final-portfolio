4ab7f31963ccd264b5c69a02d3f3a4c70adabe65
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-04 00:19:41 -0300

    feat(images): Add image for Contact component

--- app/components/Contact/ContactSide.tsx
+++ app/components/Contact/ContactSide.tsx
@@ -1,9 +1,16 @@
 import { FaWhatsapp as WhatsappIcon } from "react-icons/fa";
 import styles from '../../styles/Contact.module.css'
+import Image from 'next/image'
 export default function ContactSide(){
     return(
         <div className={styles.contactSide}>
-            <div className={styles.contactSide__image}></div>
+            <Image
+                className={styles.contactSide__logo}
+                src={'/contact-image.png'}
+                alt='contact-image'
+                width={250}
+                height={250}
+            />
             <div className={styles.cardMessage}>
                 <h4>Cotiza Rapido por WhatsApp</h4>
                 <p>Escribenos ahora y cotiza tu Pagina Web</p>
