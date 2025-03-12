44cc565d0e795aaa07868c66eeeb390eca7606b4
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-12 19:29:43 -0300

    style(Stats): Add basic style for Stats component

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Skills from "./components/Skills/Skills";
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import Stats from "./components/Stats/Stats";
 export default function Home() {
   return (
     <main className={styles.page}>
       <Hero/>
+      <Stats/>
       <Skills/>
       <Projects/>
       <Contact/>
