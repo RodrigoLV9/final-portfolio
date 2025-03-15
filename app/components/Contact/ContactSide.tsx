4c129c7e2cfcd67d151e16fddcde8e9049745fae
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-15 17:17:17 -0300

    style(ContactSide): Add style of color gradient effect to cardMessage__title

--- app/components/Contact/ContactSide.tsx
+++ app/components/Contact/ContactSide.tsx
@@ -5,10 +5,18 @@ export default function ContactSide(){
     const {language}=useLanguage()
     return(
         <div className={styles.contactSide}>
+            <svg width="0" height="0">
+                <defs>
+                    <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                        <stop offset="0%" stopColor="#25D366" />
+                        <stop offset="100%" stopColor="#128C7E" />
+                    </linearGradient>
+                </defs>
+            </svg>
             <div className={styles.cardMessage}>
                 <div className={styles.cardMessage__title}>
-                    <h2>Whatsapp</h2>
                     <WhatsappIcon/>
+                    <h2>Whatsapp</h2>
                 </div>
                 <h4>{language ? 'Cotiza Rapido por WhatsApp' : 'Get a quick quote via WhatsApp'}</h4>
                 <p>{language ? 'Escribenos ahora y cotiza tu Pagina Web':'Write to us now and get a quote for your website'}</p>
