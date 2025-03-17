10abe6438eb6695b9916ead872f6538c35d86313
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-03-16 22:21:01 -0300

    feat(menu): Add funcionality for menu and add basic responsive

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 "use client"
+import {useState} from 'react'
 import HeaderControls from './HeaderControls'
 import styles from '../../styles/Header.module.css'
 import Image from 'next/image'
 import { useLanguage } from '@/app/Contexts/LanguageContext'
 export default function Header(){
     const {language}=useLanguage()
+    const [isMenu, setIsMenu]=useState<boolean | undefined>(false)
+    const handleMenu=()=>{
+        setIsMenu(prev=>!prev)
+        
+    }
     return(
         <header className={styles.header}>
             <Image
@@ -14,16 +20,16 @@ export default function Header(){
                 width={167}
                 height={67}
             />
-            <nav className={styles.headerNav}>
+            <nav className={`${styles.headerNav} ${isMenu ? styles.nav_enabled : styles.nav_disabled}`}>
                 <a href="#home">{language ? 'Inicio' : 'Home'}</a>
                 <a href="#skills">{language ? 'Habilidades' : 'Skills'}</a>
                 <a href="#projects">{language ? 'Proyectos' : 'Projects'}</a>
                 <a href="#contact">{language ? 'Contacto' : 'Contact'}</a>
             </nav>
             <HeaderControls/>
-            <div className={styles.containerMenu}>
+            <button className={`${styles.containerMenu} ${isMenu ? styles.menu_disabled : styles.menu_enabled}`} onClick={handleMenu}>
                 <div className={styles.menu}></div>
-            </div>
+            </button>
         </header>
     )
 }
