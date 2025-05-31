94d2ed491bbdc0b4a72c02717609b5eba06aafd2
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-05-30 23:55:56 -0300

    feat(BgParticles): Create a BgParticles component in section Hero component and install dependences for BgParticles

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,11 +1,13 @@
 import HeroInfo from "./HeroInfo"
 import HeroImage from "./HeroImage"
 import styles from '../../styles/Hero.module.css'
+import { BgParticles } from "./BgParticles"
 export default function Hero(){
     return(
         <section className={styles.hero} id='home'>
             <HeroInfo/>
             <HeroImage/>
+            <BgParticles/>
         </section>
     )
 }
